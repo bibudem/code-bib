8941cfede687b5328d523debf2ed409beb316519
Christian Rémillard <christian.remillard@umontreal.ca>
2026-09-08 15:21:39 -0400

    feat: convertir en ESM
    - Ajouter "type": "module" à package.json
    - Convertir index.js pour utiliser la syntaxe d'import/export ES6 avec assertions JSON

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-module.exports = require('./bib.json')
-module.exports.codeBib = require('./bib.json')
-module.exports.codeBibSchema = require('./openapi.json')
+import bibData from './bib.json' assert { type: 'json' }
+import bibSchema from './openapi.json' assert { type: 'json' }
+
+export default bibData
+export const codeBib = bibData
+export const codeBibSchema = bibSchema
